2a20522cfb837e37663bac18d3ca2f4cda1dde75
Kamps-Duac <Kamps.duac@gmail.com>
2016-02-08 13:49:56 -0500

    working homegrownt jwt maker

--- api/api.js
+++ api/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var User = require('./models/User.js');
+var jwt = require('./services/jwt.js');
 
 var app = express();
 
@@ -15,20 +16,31 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.post('/register', function(req,res){
+app.post('/register', function(req, res) {
 	var user = req.body;
 	var newUser = new User.model({
 		email: user.email,
 		password: user.password
 	});
-	
+
+	var payload = {
+		iss: req.hostname,
+		sub: user._id
+	}
+
+	var token = jwt.encode(payload, "shhh..");
+
 	newUser.save(function(err) {
-		res.status(200).json(newUser);
+		res.status(200).send({
+			user: newUser.toJSON(),
+			token: token
+		});
 	})
 })
 
 mongoose.connect('mongodb://localhost/psjwt');
-//mongodb://<jwt_ps2>:<Jwt#8892>@ds031972.mongolab.com:31972/jwt_ps
+//mongoose.connect('mongodb://jwt_ps2:Jwt#8892@ds031972.mongolab.com:31972/jwt_ps');
+
 
 var server = app.listen(3000, function() {
 	console.log('api listening on ', server.address().port);
